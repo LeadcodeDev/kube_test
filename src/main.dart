e4c382c824b4b155d314cc593207114a69b68f6b
Baptiste Parmantier <baptiste.parmantier.pro@gmail.com>
2024-10-03 00:05:39 +0200

    feat: add features

--- src/main.dart
+++ src/main.dart
@@ -2,12 +2,16 @@ import 'package:mineral/api.dart';
 import 'package:mineral_cache/providers/memory.dart';
 
 void main(_, port) async {
-  final client = Client()
-      .setCache((e) => MemoryProvider())
-      .setHmrDevPort(port)
-      .build();
+  final client = Client().setCache((e) => MemoryProvider()).setHmrDevPort(port).build();
 
   client.events.ready((bot) => client.logger.info('${bot.username} is ready !  🚀'));
+  client.events.server
+      .serverCreate((server) => client.logger.info('Server created: ${server.name}'));
+
+  client.events.server.messageCreate((message) async {
+    if (message.author.isBot) return;
+    await message.reply(content: 'Pong !');
+  });
 
   await client.init();
 }
